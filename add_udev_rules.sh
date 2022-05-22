ea001ac9037165fae3dea6d2f7ca1bc45704e83b
David Weis <davidmichaelweis@gmail.com>
2022-05-22 04:15:22 +0100

    Add script for udev rules

---
+++ add_udev_rules.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+readonly udev_path="/etc/udev/rules.d/40-blinds.rules"
+
+echo "writing udev rules to $udev_path"
+
+cat <<EOT | sudo tee $udev_path > /dev/null
+
+# CH340
+KERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE:="0777", SYMLINK+="blinds_motors"
+
+EOT
+
+sudo udevadm control --reload-rules && udevadm trigger
+echo "Done"
