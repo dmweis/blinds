0472c4eeec9348da091cb0aaacae8fc2102b5c25
David Weis <davidmichaelweis@gmail.com>
2022-06-04 15:35:23 +0100

    Remove unused import

--- src/driver.rs
+++ src/driver.rs
@@ -5,7 +5,7 @@ use crate::{
 use anyhow::Result;
 use async_trait::async_trait;
 use log::*;
-use std::path::{Path, PathBuf};
+use std::path::{Path};
 use std::time::Duration;
 use tokio::time::sleep;
 
