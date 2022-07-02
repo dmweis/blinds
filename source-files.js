5c3d3bb341ead5c069bbb2a74f3a19cc645c36a5
dmweis <dmweis@users.noreply.github.com>
2022-07-02 19:55:10 +0000

    deploy: 151f9df25a32120d95c7bed579b12e80f9250738

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var sourcesIndex = {};
-sourcesIndex["blinds"] = {"name":"","files":["config.rs","driver.rs","error.rs","main.rs","mqtt_server.rs","routes.rs"]};
+sourcesIndex["blinds"] = {"name":"","dirs":[{"name":"driver","files":["bedroom_blinds.rs","living_room_blinds.rs","mod.rs"]}],"files":["config.rs","error.rs","main.rs","mqtt_server.rs","routes.rs"]};
 createSourceSidebar();
